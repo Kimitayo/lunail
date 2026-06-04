04d421734d5ec468c7c784c2196964ab8689de53
Kimitayo <clarinnha2vns@gmail.com>
2026-06-04 20:23:31 -0300

    adiciona UsuarioService com cadastro e login

--- lunail-api/src/main/java/com/lunail/lunail_api/repository/UsuarioRepository.java
+++ lunail-api/src/main/java/com/lunail/lunail_api/repository/UsuarioRepository.java
@@ -3,8 +3,13 @@ package com.lunail.lunail_api.repository;
 import com.lunail.lunail_api.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository  // criar uma interface pra falar diretamente com o bd
     extends JpaRepository<Usuario, String> { // como id do Usuario é string, ai deixo string aqui no seg parametro
-
+    Optional<Usuario> findByEmail(String email);
+    /*optional pra representar um valor que pode ou não ser encontrado
+    findByEmail pra encontrar um email no bd
+    * */
 }
 
---
+++ lunail-api/src/main/java/com/lunail/lunail_api/service/UsuarioService.java
@@ -0,0 +1,61 @@
+package com.lunail.lunail_api.service;
+
+import com.lunail.lunail_api.model.Usuario;
+import com.lunail.lunail_api.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+    private final UsuarioRepository usuarioRepository; // no service conecto ao repository
+
+    // construtor
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    // metodos
+    // cadastrar
+    public Usuario cadastrar(String nomeCompleto, String email, String senha, String confirmarSenha) {
+        // 1. verifica se email já existe
+        // 2. se existe -> lança erro
+        // 3. se não existe, aceita email, compara as senhas -> cria e salva
+        // 4. guarda data de cadastro
+
+        // verificar senhas
+        if (!senha.equals(confirmarSenha)) {
+            throw new RuntimeException("As senhas não coincidem");
+        }
+
+        // verificar se email já existe
+        Optional<Usuario> optional = usuarioRepository.findByEmail(email);
+        if (optional.isPresent()) {
+            throw new RuntimeException("Este e-mail já existe");
+        } else {
+            Usuario novoUsuario = new Usuario(nomeCompleto, email, senha);
+            return usuarioRepository.save(novoUsuario); // salva no banco de dados e retorna
+        }
+    }
+
+    // login
+    public Usuario login(String email, String senha) {
+        // 1. busca usuario pelo email
+        // 2. se não existe -> lança erro
+        // 3. se existe -> verifica senha
+        // 4. se senha errada -> lança erro
+        // 5. se tudo certo -> retorna usuário
+
+        Optional<Usuario> optional = usuarioRepository.findByEmail(email);
+        if (optional.isEmpty()) { // email nao existe
+            throw new RuntimeException("Email ou senha incorretos");
+        } else {
+            Usuario usuario = optional.get(); // se tem algo, pega o que tem dentro
+            if (!senha.equals(usuario.getSenha())) { // senha diferente do getSenha()
+                throw new RuntimeException("Email ou senha incorretos");
+            } else {
+                return usuario;
+            }
+        }
+    }
+}
